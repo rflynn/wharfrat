c5e889701d6f41b562ff36654744981b6b27aeeb
Ryan Flynn <parseerror+github@gmail.com>
2015-06-07 16:47:18 -0400

    non-blocking stdout/stderr streams

--- wharfrat/interface.py
+++ wharfrat/interface.py
@@ -1,11 +1,36 @@
-from subprocess import check_output, STDOUT, CalledProcessError
+from subprocess import Popen, STDOUT, PIPE, CalledProcessError
 import sys
+
 
 def issue(command):
     try:
-        response = check_output(command, stderr=STDOUT, shell=True)
+        p = Popen(command,
+                  stdout=PIPE,
+                  stderr=PIPE,
+                  shell=True)
+        response = handle_output(p)
+        e = p.returncode
+        if e:
+            raise CalledProcessError(e, command, output=response)
     except CalledProcessError as e:
         print e.output
         raise e
-    print response
     return response
+
+
+def handle_output(p):
+    response = []
+    try:
+        c = p.communicate()
+        while c:
+            out, err = c
+            if out:
+                response.append(out)
+                sys.stdout.write(out)
+            if err:
+                response.append(err)
+                sys.stderr.write(err)
+            c = p.communicate()
+    except ValueError as e:
+        pass # .communicate() on dead process, it's fine...
+    return ''.join(response)
