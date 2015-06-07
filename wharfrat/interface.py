3426fc8483e757ffa86f65e18320b4d93f4fad0d
Ryan Flynn <parseerror+github@gmail.com>
2015-06-07 18:25:56 -0400

    non-blocking stdout/stderr

--- wharfrat/interface.py
+++ wharfrat/interface.py
@@ -1,11 +1,51 @@
-from subprocess import check_output, STDOUT, CalledProcessError
+from subprocess import Popen, STDOUT, PIPE, CalledProcessError
 import sys
+
+# FIXME: not portable
+from fcntl import fcntl, F_GETFL, F_SETFL
+from os import O_NONBLOCK
+from select import select
+import errno
 
 def issue(command):
     try:
-        response = check_output(command, stderr=STDOUT, shell=True)
+        p = Popen(command,
+                  stdout=PIPE,
+                  stderr=PIPE,
+                  shell=True)
+        response = handle_output(p)
+        e = p.returncode
+        if e:
+            raise CalledProcessError(e, command, output=response)
     except CalledProcessError as e:
         print e.output
         raise e
-    print response
+    try:
+        p.close()
+    except:
+        pass
     return response
+
+def handle_output(p):
+
+    def unblock_file(f):
+        flags = fcntl(f, F_GETFL)
+        fcntl(f, F_SETFL, flags | O_NONBLOCK)
+
+    response = []
+    try:
+        unblock_file(p.stdout)
+        unblock_file(p.stderr)
+        exitcode = p.poll()
+        while exitcode is None:
+            readable, _, _ = select([p.stdout, p.stderr], [], [], 0.125)
+            for r in readable:
+                try:
+                    x = r.read()
+                    response.append(x)
+                    sys.stdout.write(x)
+                except: pass
+            exitcode = p.poll()
+    except:
+        raise
+    return ''.join(response)
